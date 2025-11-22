699f2aee4bfb18e3426ac1d588e2a01479d5ccf0
edzanko1 <edzanko1@etf.unsa.ba>
2025-11-22 16:53:30 +0100

    Added logging service

---
+++ EntrioX/Extensions/ServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Contracts;
+using LoggerService_;
+
+namespace EntrioX.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
+}
