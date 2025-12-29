399f4ebea36a01f5b741b60cbf6ae9f3839192bd
Ubuntu <ubuntu@ip-172-31-7-253.ec2.internal>
2025-12-29 17:46:31 +0000

    fixed auth service

--- Service/AuthenticationService.cs
+++ Service/AuthenticationService.cs
@@ -219,10 +219,11 @@ namespace Service
 
         private SigningCredentials GetSigningCredentials()
         {
-            var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable(GetSecret()));
-            var secret = new SymmetricSecurityKey(key);
-            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
-        }
+            var secret = GetSecret();
+            Console.WriteLine(secret);
+            var key = Encoding.UTF8.GetBytes(secret);
+            return new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+  }
 
         private async Task<List<Claim>> GetClaims()
         {
@@ -318,8 +319,10 @@ namespace Service
         }
 
         private static string GetSecret()
-        {
+        {  
+            Console.WriteLine("TUUUUUUU SAAAAAM");
             var secret = Environment.GetEnvironmentVariable("SECRET");
+            Console.WriteLine(secret);
             if (string.IsNullOrWhiteSpace(secret))
                 throw new Exception("JWT SECRET is not configured");
             return secret;
